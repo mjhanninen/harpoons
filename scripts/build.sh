7b4ffe9490ede602e43561a77e334285a201a0ff
Matti Hänninen <matti@codebrutale.com>
2019-04-19 06:52:37 +0300

    Add build scripts, use non-interactive Clojure CLI

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 [[ "${CI-}" == "true" ]] && set -x
 cd "$(git rev-parse --show-toplevel)"
-clojure -A:test:1.7
-clojure -A:test:1.8
-clojure -A:test:1.9
-clojure -A:test:1.10
+[[ -e target/ ]] || mkdir target
+clojure -A:build
